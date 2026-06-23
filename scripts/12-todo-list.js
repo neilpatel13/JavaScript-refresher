568ad401da0202b3593218768a1bd2e006d54c89
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2026-06-23 18:58:16 -0400

    Completed P12 P2

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -12,25 +12,34 @@ function rendertodolist() {
 
     let htmltodo = '';
 
-    todolist.forEach(function (todoObject, index) {
+    todolist.forEach((todoObject, index) =>  {
 
         const { name, dueDate } = todoObject;
         const html = `
             <div>${name}</div>
             <div>${dueDate}</div>
-            <button onclick="
-                todolist.splice(${index}, 1);
-                rendertodolist();
-            " class="delete-todo-button">Delete</button>
+            <button class="delete-todo-button js-delete-todo-button">Delete</button>
         `;
         htmltodo += html
     });
 
     document.querySelector('.js-todo-list')
         .innerHTML = htmltodo;
+    
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                todolist.splice(index, 1);
+                rendertodolist();
+            });
+        });
 }
 
 
+document.querySelector('.js-add-todo-button')
+    .addEventListener('click', () => {
+        addTodo();
+    });
 
 function addTodo() {
     const inputElement = document.querySelector('.js-nameinput');
